6313a913d33ac64df080e1d64bb0bd03fd0b7823
Pamela Rodrigues <akahime2014@gmail.com>
2025-08-08 00:46:31 -0300

    Basic Page Structure

---
+++ src/app/components/staticstickers/staticstickers.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-staticstickers',
+  imports: [],
+  templateUrl: './staticstickers.html',
+  styleUrl: './staticstickers.css'
+})
+export class Staticstickers {
+
+}
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {Maincontent} from '../../components/maincontent/maincontent';
+import {Title} from '../../components/title/title';
+import {Staticstickers} from '../../components/staticstickers/staticstickers';
+import {Stickers} from '../../components/stickers/stickers';
+
+@Component({
+  selector: 'app-home',
+  imports: [Maincontent,Title,Staticstickers,Stickers],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
