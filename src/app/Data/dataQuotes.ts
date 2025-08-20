0708d17ceb3557b0ba67e0d076397d39f8aa4909
Pamela Rodrigues <akahime2014@gmail.com>
2025-08-20 14:52:27 -0300

    Data Updated

--- src/app/Data/dataQuotes.ts
+++ src/app/Data/dataQuotes.ts
@@ -1,26 +1,39 @@
 export const dataQuotes = [
   {
     "id":"1",
-    "title":"TÍTULO QUOTE",
-    "text":"There is no strife, no prejudice, no national conflict in outer space as yet. Its hazards are hostile to us all. Its conquest deserves the best of all mankind, and its opportunity for peaceful cooperation many never come again. But why, some say, the moon? Why choose this as our goal? And they may well ask why climb the highest mountain? Why, 35 years ago, fly the Atlantic? Why does Rice play Texas?",
-    "comment": "Comentário da citação."
+    "title":"O pôr do sol",
+    "text":`Um dia, eu vi o sol se pôr quarenta e quatro vezes! - E um pouco depois acrescentou:
+    — Você sabe... Quando estamos tristes demais, gostamos de ver o sol se pôr.
+    — No dia em que viu quarenta e quatro vezes, você estava tão triste assim?
+    Mas o pequeno príncipe não respondeu.`,
+    "comment": "O Antoine deve ter passado muito tempo naquele avião vendo pores do sol e sentindo saudades."
   },
   {
     "id": "2",
-    "title":"TÍTULO MANEIRO",
-    "text":"But why, some say, the moon?",
-    "comment":"Comentário da citação. 2"
+    "title":"Um exemplar em milhões e milhões de estrelas",
+    "text":`Se alguém ama uma flor da qual só existe um exemplar em milhões e milhões de estrelas, isso basta para que seja feliz quando as olha.
+    Ele diz a si mesmo: "Minha flor está aí em algum lugar..."
+    Mas se o carneiro come a flor, é para ele como se, bruscamente, todas as estrelas do mundo se apagassem! E isso não é importante!`,
+    "comment":"--"
   },
   {
     "id": "3",
-    "title":"TITULO BOOK",
-    "text":"Why, 35 years ago, fly the Atlantic?",
-    "comment":"Comentário da citação. 3"
+    "title":"O piloto tenta consolar o pequeno príncipe",
+    "text": `Eu não sabia mais o que dizer. Eu me sentia muito desconfortável. Eu não sabia como chegar até ele, onde buscá-lo...
+    É misterioso demais o país das lágrimas!`,
+    "comment":`O piloto fez o pequeno príncipe chorar, porque ignorou o que era importante para ele.
+    Achou que suas coisas eram prioridade, e os assuntos da criança eram besteira.`
   },
   {
     "id": "4",
-    "title":"TITULO RAPOSA",
-    "text":"Its conquest deserves the best of all mankind, and its opportunity for peaceful cooperation many never come again.",
-    "comment":"Comentário da citação. 4"
+    "title":"Despedida",
+    "text":`— Adeus - repetiu ele.
+    A flor tossiu. Mas não foi por causa do resfriado.
+    — Eu fui estúpida - disse ela enfim. — Peço perdão a você. Trate de ser feliz.
+    Ele ficou surpreso com a ausência de reprovações.
+    Lá ficou todo desconcertado, a cúpula no ar. Não compreendia essa doçura calma.
+    — Mas, sim, eu amo você - disse a flor. - Você não sabia de nada por minha culpa. Isso não tem nenhuma importância.
+    Mas você é tão tonto quanto eu. Trate de ser feliz...`,
+    "comment":`Despedidas são tristes.`
   }
   ]
