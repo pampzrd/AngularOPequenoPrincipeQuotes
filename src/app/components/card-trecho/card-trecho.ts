ef65e30ad2a45c41393eade85fb087ae600bbef1
Pamela Rodrigues <akahime2014@gmail.com>
2025-08-22 19:12:54 -0300

    update

--- src/app/components/card-trecho/card-trecho.ts
+++ src/app/components/card-trecho/card-trecho.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
-import {RouterLinkActive} from '@angular/router';
+import {Component, Input} from '@angular/core';
+import {ActivatedRoute, RouterLinkActive} from '@angular/router';
+import {dataQuotes} from '../../Data/dataQuotes';
 
 @Component({
   selector: 'app-card-trecho',
   imports: [
-    RouterLinkActive
+    RouterLinkActive,
+    dataQuotes
   ],
   templateUrl: './card-trecho.html',
   styleUrl: './card-trecho.css'
 })
 export class CardTrecho {
+  @Input()
   private id :string|null="";
   tituloCard:string="";
+
+  constructor(private route:ActivatedRoute) {
+    this.route.paramMap.subscribe(value=>this.id=value.get("id"));
+    this.pegarTituloPeloId(this.id)
+  }
+  //méto do para atribuir um id para pegar o conteudo desse id do "banco"
+  pegarTituloPeloId(id:string|null){
+    this.id=id;
+    const data = dataQuotes.filter(trecho=>trecho.id==id)[0]
+    this.tituloCard=data.title;
+  }
 
 }
